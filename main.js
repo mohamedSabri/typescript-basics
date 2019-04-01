8588416c5d712e6e71c026159c55ef8a19380364
mohamed_sabri <mohamed.sabri@visionvalley.net>
2019-04-01 09:57:57 +0200

    015 Declaring Variables

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-//014 Your First TypeScript Program
+//viedo number 014 Your First TypeScript Program
 function log(message) {
     console.log(message);
 }
 var message = "Hello World";
 log(message);
+//video number 015 Declaring Variables
+var number = 1;
+var count = 2;
+//difference between var and let
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    // the variable i is avaliable here as it is avaliable to the nearest function scope function (inside doSomething() method)
+    console.log('Finally: ' + i);
+}
+doSomething();
+function doSomethingWithLet() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    // the variable i is not avaliable here as it is avaliable to the nearest block scope 
+    //(inside for loop) but it still compile well even with compilation error and generate main.js with var instead of let as it compiles to ECMAScript5
+    console.log('Finally: ' + i);
+}
+doSomethingWithLet();
