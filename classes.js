8c5662f6509b7b4f8354813f978c47ab937579a9
mohamed_sabri <mohamed.sabri@visionvalley.net>
2019-04-01 13:33:08 +0200

    020 Classes

---
+++ classes.js
@@ -0,0 +1,12 @@
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    // methods 
+    Point.prototype.draw = function () {
+        // ...
+    };
+    Point.prototype.getDistance = function (another) {
+        // ...
+    };
+    return Point;
+}());
---
+++ classes.ts
@@ -0,0 +1,14 @@
+class Point {
+    // members or fields
+    x: number;
+    y: number;
+
+    // methods 
+    draw(){
+        // ...
+    }
+
+    getDistance(another: Point){
+        // ...
+    }
+}
